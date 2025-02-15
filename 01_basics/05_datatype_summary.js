66e9492352349b22b495006a73339ace4308336a
Manish Kumar <manishkumar72313180@gmail.com>
2025-02-15 06:25:52 +0000

    container changing

---
+++ 01_basics/05_datatype_summary.js
@@ -0,0 +1,14 @@
+// primitive 
+
+const score = 100
+const scoreValue = 100.33   // in js decimal values and float values are considered as numbers #same //
+
+
+const isLoggedin = false
+const outsidetemp = null
+let userEmail;
+
+const id= Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
