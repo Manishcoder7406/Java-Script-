0bed19a6816f2f1843fa9dc85c8048597b3f4675
Manish Kumar <manishkumar72313180@gmail.com>
2025-02-22 10:02:38 +0000

    managing container

---
+++ 01_basics/06_memory.js
@@ -0,0 +1,10 @@
+// let n1=10;                       stack memory 
+// let n2=n1;
+// n2=20;
+// console.log(n1);              output 10
+
+// let obj1 = {name:"Manish"};
+// let obj2 =obj1;
+// obj2.name ="Mani";                          Heap memory
+// console.log(obj1.name);           output : Mani
+
