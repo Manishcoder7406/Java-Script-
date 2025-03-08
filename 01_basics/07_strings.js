068090160aa16567e6b2f574edd262be09c54595
Manish Kumar <manishkumar72313180@gmail.com>
2025-03-08 08:48:46 +0000

    container changes

---
+++ 01_basics/07_strings.js
@@ -0,0 +1,27 @@
+// console.log("manish");
+
+const name= "Manish"
+const repocount =100
+
+console.log(name + repocount + "Value");
+
+console.log(`Hello my name is ${name} and my repocount is ${repocount}`);
+
+const game_name= new String('Manish');
+console.log(game_name);
+
+
+console.log(game_name[0])
+console.log(game_name.__proto__);
+
+console.log(game_name.toUpperCase());  //operations on strings//
+console.log(game_name.length);
+
+console.log(game_name.charAt(2));
+
+const newString ='   Manish   ';
+
+const url ="https://manish.com/manish%20kumar"
+console.log(url.replace('%20','-'))
+
+
