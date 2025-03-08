009ebed808550f061038c5460effd2d3d54694a9
Manish Kumar <manishkumar72313180@gmail.com>
2025-03-08 11:38:11 +0000

    maths

---
+++ 01_basics/08_numbers.js
@@ -0,0 +1,33 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance)
+
+console.log(balance.toString() );
+
+const otherNumber = 123.8966
+
+console.log(otherNumber.toPrecision(4))
+
+let num= 13.3718
+let n = num.toPrecision(2)
+console.log(n)
+
+const hundreds = 100000000
+console.log(hundreds.toLocaleString())
+//
+
+
+// to import maths we use 
+console.log(Math)
+
+console.log(Math.abs(-4))
+console.log(Math.round(4.6))
+
+
+console.log(Math.ceil(5.6))
+console.log(Math.floor(3.4))
+
+console.log((Math.random()*10) +1)
+console.log((Math.random()*100) +1)
