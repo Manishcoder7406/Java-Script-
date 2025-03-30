c54b9f0647800290d9175b829b7f578307c30613
Manish Kumar <manishkumar72313180@gmail.com>
2025-03-30 15:58:18 +0000

    s

--- 01_basics/09_dates.js
+++ 01_basics/09_dates.js
@@ -1,8 +1,33 @@
 // dates are managed in milliseconds
 
-let mydate = new Date()
-console.log(mydate.toString());
-console.log(mydate.toLocaleString());
-console.log(mydate.toTimeString ());
+// let mydate = new Date()
+// console.log(mydate.toString());
+// console.log(mydate.toLocaleString());
+// console.log(mydate.toTimeString ());
 
-console.log(typeof mydate);     // type of date is OBJECT //
+//  console.log(typeof mydate);     // type of date is OBJECT //
+
+
+// let myCreatedDate = new Date(2023,0,23)
+
+// console.log(myCreatedDate.toDateString())
+
+// let myCreatedDate = new Date("2023-01-23")
+let myCreatedDate = new Date("01-10-2025")
+
+//  console.log(myCreatedDate.toLocaleString())
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp)     // used to show the milliseconds from 1st jan 1970 to till now
+
+//console.log(myCreatedDate.getTime());
+
+// to convert in seconds
+console.log(Date.now()/1000)   // ths will give answer in decimal form
+// to overcome the decimal we use math.floor
+
+console.log(Math.floor(Date.now()/1000));
+
+// there are many methods which are used to get the hours , minutes, seconds, days , years etc 
+// .get...()  like this 
+
