8a72044c0d41f8d83707cdceeecb02561c4123db
Manish Kumar <manishkumar72313180@gmail.com>
2025-03-30 14:58:59 +0000

    date

---
+++ 01_basics/09_dates.js
@@ -0,0 +1,8 @@
+// dates are managed in milliseconds
+
+let mydate = new Date()
+console.log(mydate.toString());
+console.log(mydate.toLocaleString());
+console.log(mydate.toTimeString ());
+
+console.log(typeof mydate);     // type of date is OBJECT //
